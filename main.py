2c0e5a67dbebf585839b05c17059ab149a89729e
Onyedika Christopher <cosimon.tech@gmail.com>
2026-09-10 08:54:22 +0100

    feat: add task creation and listing endpoints

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from sqlmodel import Session, select
 from database import create_db_and_tables, get_session
 from dependencies import require_api_key, PaginationParams
-from model import User, UserCreate, UserPublic
+from model import User, UserCreate, UserPublic, Task, TaskCreate, TaskPublic
 
 app =  FastAPI(title="Task Management API")
 
@@ -28,6 +28,24 @@ async def create_user(
     session.refresh(db_user)
     return db_user
 
+async def create_task(
+        task: TaskCreate,
+        session: Session = Depends(get_session),
+        api_key: str = Depends(get_session),
+):
+    user = session.get(User, task.user_id)
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
+    db_task = Task.model_validate(task)
+    session.add(db_task)
+    session.commit()
+    session.refresh(db_task)
+    return db_task
+
 @app.get("/users", response_model=list[UserPublic])
 def get_users(
     session: Session = Depends(get_session),
@@ -41,3 +59,17 @@ def get_users(
     users = session.exec(statement).all()
     return users
 
+@app.get("/tasks", response_model=list[TaskPublic])
+async def get_tasks(
+    session: Session = Depends(get_session),
+    pagination: PaginationParams = Depends(PaginationParams),
+):
+    statement = (
+        select(Task)
+        .offset(pagination.offset)
+        .limit(pagination.limit)
+    )
+
+    tasks = session.exec(statement).all()
+
+    return tasks
